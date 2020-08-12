3a856f2a2db84d5ae5205ac3fc3514ae2dd57743
Jared <j95grow@gmail.com>
2020-08-11 20:56:16 -0400

    Connect repair items to adapter

--- app/src/main/java/com/yagr/scv/bindings/Bindings.kt
+++ app/src/main/java/com/yagr/scv/bindings/Bindings.kt
@@ -2,8 +2,17 @@ package com.yagr.scv.bindings
 
 import androidx.recyclerview.widget.RecyclerView
 import androidx.databinding.BindingAdapter
+import com.yagr.scv.data.Repair
+import com.yagr.scv.repairlist.RepairListAdapter
 
 @BindingAdapter("adapter")
 fun bindAdapter(view: RecyclerView, adapter: RecyclerView.Adapter<*>) {
     view.adapter = adapter
+}
+
+@BindingAdapter("repairItems")
+fun bindAdapter(view: RecyclerView, items: List<Repair>?) {
+    items?.let {
+        (view.adapter as RepairListAdapter).updateItems(items)
+    }
 }
--- app/src/main/java/com/yagr/scv/repairlist/RepairListAdapter.kt
+++ app/src/main/java/com/yagr/scv/repairlist/RepairListAdapter.kt
@@ -2,17 +2,23 @@ package com.yagr.scv.repairlist
 
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.yagr.scv.data.Repair
 import com.yagr.scv.databinding.ItemRepairBinding
 
 class RepairListAdapter : RecyclerView.Adapter<RepairListAdapter.RepairViewHolder>() {
 
+    private val items = mutableListOf<Repair>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepairViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount() = items.size
+
+    fun updateItems(repairs: List<Repair>) {
+        items.clear()
+        items.addAll(repairs)
+        notifyDataSetChanged()
     }
 
     override fun onBindViewHolder(holder: RepairViewHolder, position: Int) {
