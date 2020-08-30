701211358fa1b66b8c2ec386bcca90e6aedaacd1
Jared <j95grow@gmail.com>
2020-08-30 17:46:40 -0400

    Repair detail screen initial creation

---
+++ app/src/main/java/com/yagr/scv/repairdetail/RepairDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.yagr.scv.repairdetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.yagr.scv.databinding.FragmentRepairDetailBinding
+import com.yagr.scv.utility.ViewModelFactory
+
+class RepairDetailFragment : Fragment() {
+
+    private val viewModel: RepairDetailViewModel by viewModels {
+        ViewModelFactory(requireActivity().application, this)
+    }
+
+    lateinit var binding: FragmentRepairDetailBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRepairDetailBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewmodel = viewModel
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/yagr/scv/repairdetail/RepairDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.yagr.scv.repairdetail
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.yagr.scv.data.RepoCreator
+
+class RepairDetailViewModel(application: Application) : AndroidViewModel(application) {
+}
